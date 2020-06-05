d44148b34837a9e6dcd6995ce9439ae2f7d5c165
ainanicolau <ainaa.nic@gmail.com>
2020-06-06 00:06:18 +0100

    Defining padding parameters

--- create_landscape.py
+++ create_landscape.py
@@ -8,54 +8,66 @@ import numpy as np
 import midpoint_displacement as md
 
 # Image Resolution
-WIDTH = 513
-HEIGHT = 720
+WIDTH = 420
+HEIGHT = 595
 IMAGE_PADDING = 50
 
-
-
-# Colour Palettes
-COLOUR_PALETTES = {"test":[(0,0,255,255),(0,0,150,255), (0,0,50,255)]}
-# COLOUR_PALETTES = {"terracotta":{"sun":(162, 102, 81, 4), "sky":(216, 195, 165, 4),
-                   # "land":[(171, 122, 106, 4), (44, 34, 25, 4)]}}
-
+# Color Palettes
+COLOR_PALETTES = { "Terracotta":{"sun":(60, 83, 147, 255), "sky":(163, 196, 220, 255),
+                   "land":[(106, 122, 171, 255), (25, 34, 44, 255)]},
+                   "Desert":{"sun":(125, 187, 227, 255), "sky":(175, 206, 229, 255),
+                   "land":[(44, 67, 129, 255)]},
+                   "Retro":{"sun":(201, 222, 237, 255), "sky":(210, 182, 88, 255),
+                   "land":[(50, 59, 222, 255), (26, 138, 232, 255)]}}
 # Texture files
 TEX = "paper.png"
 
 
-def generate_image(width, height):
+def generate_image(width, height, color):
 
     image = np.zeros((height, width, 4), np.uint8)
-    image[:, :] = (255, 255, 255, 255)
+    image[:, :] = color
 
     return image
 
 
-def draw_sun(image, radius, center_x, center_y):
+def draw_sun(image, radius, center_x, center_y, color):
     center = (center_x,center_y)
-    colour = (0,0,0, 255)
 
     if radius > 0:
-        cv2.circle(image, center, radius, colour, thickness=-1, lineType=8, shift=0)
+        cv2.circle(image, center, radius, color, thickness=-1, lineType=8, shift=0)
 
     return image
 
 
-def generate_mountains(image, num_layers, roughness, width, height):
+def generate_mountains(image, num_layers, roughness, width, height, upper_padding):
     mountains = []
     for layer in range(num_layers):
         layer_roughness = roughness // (layer + 1)
         layer_heights = md.run_midpoint_displacement(layer_roughness)
 
-        layer_heights = md.normalize(layer_heights, HEIGHT - IMAGE_PADDING,
-                                  IMAGE_PADDING + layer * 200)
+        # layer_heights = md.normalize(layer_heights, HEIGHT - IMAGE_PADDING,
+        #                           IMAGE_PADDING + layer * 200)
+        # layer_heights = md.normalize(layer_heights, HEIGHT - IMAGE_PADDING,
+        #                           IMAGE_PADDING + HEIGHT/num_layers * layer)
+        # layer_heights = md.normalize(layer_heights, HEIGHT - 100,
+        #                              (HEIGHT -100-100)/num_layers * layer + 100)
+
+        # Upper padding
+        # Lower padding
+        # mountain intersection
+
+        # layer_heights = md.normalize(layer_heights, HEIGHT - 100 - ((HEIGHT -100-100)/num_layers * (num_layers-layer-1)),
+        #                             (HEIGHT -100-100)/num_layers * layer + 100) 
+        layer_heights = md.normalize(layer_heights, HEIGHT - 100 - ((HEIGHT -100-upper_padding)/num_layers * (num_layers-layer-1)),
+                                    (HEIGHT -100-upper_padding)/num_layers * layer + upper_padding) 
         mountains.append(layer_heights)
 
     return mountains
 
 
 def draw_mountains(image, mountains, imageWidth, imageHeight,    
-                  colour):
+                  color):
 
     for layer in range(len(mountains)):
         # Convert the heights into the list of points of a polygon
@@ -70,10 +82,17 @@ def draw_mountains(image, mountains, imageWidth, imageHeight,
 
         square = np.array([[10,10], [10,100], [100,100], [100, 10], [10, 10]], np.int32)
         square = square.reshape((-1,1,2))
-
-        cv2.fillPoly(image,[points],colour[layer])
+        # layer_color = len(color)>1: color[layer%2] % 
+        if len(color) > 1:
+            layer_color = color[layer%2]
+        else:
+            initial_color = list(color[0])
+            initial_color[3] = initial_color[3] / (len(mountains)) * (layer+1)
+            layer_color = tuple(initial_color)
+        cv2.fillPoly(image,[points],layer_color)
 
     return image
+
 
 def smooth_mountains(mountains, smooth_value):
     smoothed_mountains = []
@@ -97,14 +116,16 @@ class Window(QtWidgets.QMainWindow):
         # self.setCentralWidget(widget)
 
         # Attributes to build the image
-        self.__sun_radius = 5
+        self.__sun_radius = 0
         self.__center_x = 100
         self.__center_y = 100
         self.__mountain_layers = 3
         self.__roughness = 100
         self.__mountains = []
+        self.__upper_padding = 100
         self.__smoothed_mountains = []
         self.__smooth = 0
+        self.__color_palette = "Terracotta"
 
         # Sun Radius Slider
         self.__sun_radius_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
@@ -140,12 +161,30 @@ class Window(QtWidgets.QMainWindow):
         generate_mountains_layout.addWidget(self.__roughness_edit)
         generate_mountains_layout.addWidget(self.__generate_mountains_button)
 
+        # Upper padding
+        self.__upper_padding_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.__upper_padding_slider.setMinimum(0)
+        self.__upper_padding_slider.setMaximum(HEIGHT)
+        self.__upper_padding_slider.setValue(int(self.__upper_padding))
+        self.__upper_padding_slider.valueChanged[int].connect(self.on_upper_padding_changed)
+
         # Smooth mountains
         self.__smooth_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
         self.__smooth_slider.setMinimum(0)
         self.__smooth_slider.setMaximum(100)
         self.__smooth_slider.setValue(int(self.__smooth))
         self.__smooth_slider.valueChanged[int].connect(self.on_smooth_changed)
+
+        # Color Palette
+        # color_palette_combobox = self.__create_combobox(COLOR_PALETTES.keys())
+        self.__color_palette_combobox = QtWidgets.QComboBox()
+        self.__color_palette_combobox.addItems(COLOR_PALETTES.keys())
+        self.__currentIndex = list(COLOR_PALETTES.keys()).index(self.__color_palette)
+        self.__color_palette_combobox.setCurrentIndex(self.__currentIndex)
+        self.__color_palette_combobox.currentIndexChanged[int].connect(
+            self.on_color_palette_changed)
+        self.__color_palette_layout = QtWidgets.QHBoxLayout()
+        self.__color_palette_layout.addWidget(self.__color_palette_combobox)
 
 
         # Parameters Layout
@@ -157,13 +196,18 @@ class Window(QtWidgets.QMainWindow):
         parameters_layout.addWidget(QtWidgets.QLabel('Center Y'))
         parameters_layout.addWidget(center_y_slider)
         parameters_layout.addLayout(generate_mountains_layout)
+        parameters_layout.addWidget(QtWidgets.QLabel('Upper Padding'))
+        parameters_layout.addWidget(self.__upper_padding_slider)
         parameters_layout.addWidget(QtWidgets.QLabel('Smooth'))
         parameters_layout.addWidget(self.__smooth_slider)
+        parameters_layout.addWidget(QtWidgets.QLabel("Color Palette"))
+        parameters_layout.addLayout(self.__color_palette_layout)
+
 
         # Image
         self.__image_frame = QtWidgets.QLabel()
-        self.__image = np.zeros((595, 420, 4), np.uint8)
-        self.__image[:, :] = (255, 255, 255, 255)
+        self.__image = np.zeros((HEIGHT, WIDTH, 4), np.uint8)
+        self.__image[:, :] = COLOR_PALETTES[self.__color_palette]["sky"]
         qImage = QtGui.QImage(
             self.__image.data, self.__image.shape[1], self.__image.shape[0],
             QtGui.QImage.Format_ARGB32)
@@ -208,10 +252,11 @@ class Window(QtWidgets.QMainWindow):
         self.__mountains = generate_mountains(self.__image, 
                                           int(self.__mountain_layers_edit.text()), 
                                           int(self.__roughness_edit.text()), 
-                                          WIDTH, HEIGHT)
+                                          WIDTH, HEIGHT, self.__upper_padding)
         self.__smooth = 0
         self.__smooth_slider.setValue(0)
         self.__update()
+
 
     def on_smooth_changed(self, value):
         self.__smooth = value
@@ -219,15 +264,26 @@ class Window(QtWidgets.QMainWindow):
         self.__update()
 
 
+    def on_color_palette_changed(self, value):
+        self.__currentIndex = value
+        self.__color_palette = list(COLOR_PALETTES.keys())[self.__currentIndex]
+        self.__update()
+
+
+    def on_upper_padding_changed(self, value):
+        self.__upper_padding = value
+        self.__update()
+
+
     def __update(self):
 
         # Generate Image
-        self.__image = generate_image(WIDTH, HEIGHT)
+        self.__image = generate_image(WIDTH, HEIGHT, COLOR_PALETTES[self.__color_palette]["sky"])
         self.__image = draw_sun(self.__image, self.__sun_radius, self. __center_x,
-                                      self.__center_y)
+                                      self.__center_y, COLOR_PALETTES[self.__color_palette]["sun"])
         mountains = self.__smoothed_mountains if self.__smooth else self.__mountains
         self.__image = draw_mountains(self.__image, mountains, WIDTH, HEIGHT,    
-                                      COLOUR_PALETTES["test"])
+                                      COLOR_PALETTES[self.__color_palette]["land"])
 
         qImage = QtGui.QImage(
             self.__image.data, self.__image.shape[1], self.__image.shape[0],
